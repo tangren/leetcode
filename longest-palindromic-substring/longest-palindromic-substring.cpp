6146657723460f3002a106578422fa7597f725f2
tangren <tangren.zhb@gmail.com>
2014-11-03 22:36:24 +0800

    fix a bug

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -24,6 +24,7 @@ struct Pos {
 class Solution {
 public:
   string longestPalindrome(string s) {
+    cout << "***** string:" << s << endl;
     if (s.length() <= 1) {
       return s;
     }
@@ -32,13 +33,23 @@ public:
     subStrs.reserve(1000);
     char pre = s[0];
     char prepre;
-    for (int curPos = 1; curPos < s.length() - 1; curPos++) {
+    for (int curPos = 1; curPos < s.length(); curPos++) {
       char cur = s[curPos];
       if (cur == pre) {
 	Pos p;
 	p.start = curPos;
 	p.end = curPos;
 	subStrs.push_back(p);
+	
+	if (curPos > 1) {
+	  prepre = s[curPos - 2];
+	  if (cur == prepre) {
+	    Pos p;
+	    p.start = curPos - 1;
+	    p.end = curPos;
+	    subStrs.push_back(p);
+	  }
+	}
       } else {
 	if (curPos > 1) {
 	  prepre = s[curPos - 2];
@@ -72,13 +83,13 @@ public:
 	  p.complete = true;
 	}
 
-	// cout << "cur:" << cur
-	//      << ", curPos:" << curPos
-	//      << ", start:" << subStrs[i].start 
-	//      << ", end:" << subStrs[i].end << ", "
-	//      << s.substr(subStrs[i].start, 
-	// 		 subStrs[i].end - subStrs[i].start + 1) 
-	//      << ", complete:" << subStrs[i].complete << endl;
+	cout << "cur:" << cur
+	     << ", curPos:" << curPos
+	     << ", start:" << subStrs[i].start 
+	     << ", end:" << subStrs[i].end << ", "
+	     << s.substr(subStrs[i].start, 
+			 subStrs[i].end - subStrs[i].start + 1) 
+	     << ", complete:" << subStrs[i].complete << endl;
       }
     }
 
@@ -101,8 +112,11 @@ TEST(SolutionTest, testSimple) {
   Solution s;
   ASSERT_EQ("a", s.longestPalindrome("a"));
   ASSERT_EQ("aa", s.longestPalindrome("aab"));
+  ASSERT_EQ("aaa", s.longestPalindrome("aaa"));
   ASSERT_EQ("aba", s.longestPalindrome("abaa"));
   ASSERT_EQ("bcabacb", s.longestPalindrome("abcabacbcaa"));
   ASSERT_EQ("bcabbacb", s.longestPalindrome("abcabbacbcaa"));
+  ASSERT_EQ("abbbbba", s.longestPalindrome("accabbbbba"));
+  ASSERT_EQ("accabbbbbacca", s.longestPalindrome("accabbbbbacca"));
 }
 
